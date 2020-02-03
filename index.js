59fbea0f67e46167adaf8ce4ca1b80b13842e19b
front-coder <alex.mac.general@gmail.com>
2020-02-02 22:06:46 -0300

    Rotas para criação e edição
    Rotas para criação de um novo projeto e edição do título de um projeto selecionado

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
 
 //Variaveis de ambiente
 require('dotenv').config();
@@ -7,12 +8,44 @@ require('dotenv').config();
 //Porta
 const port = process.env.PORT || 3000;
 
+//Variável para armazenamento dos projetos
+const projects = [];
+
 // Lista todos os projetos
 server.get('/projects', (req, res) => {
-    return res.json({projects: 'Projetos'}); 
+    return res.json(projects); 
 });
 
-// Rota usada caso uma url não mapeada seja usada 
+// Cria um novo projeto
+server.post('/projects', (req, res) => {
+    const { id, title } = req.body;
+
+    let project = {
+        id,
+        title,
+        tasks: []
+    }
+
+    projects.push(project);
+
+    return res.json(projects); 
+});
+
+// Edita o título de um projeto existente
+server.put('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    projects.forEach((project) => {
+        if(project.id === id){
+            project.title = title
+        }
+    });
+
+    return res.json(projects); 
+});
+
+// Rota usada caso uma url não mapeada seja digitada 
 server.get('*', (req, res) => {
     return res.json({message: 'Página não encontrada'}); 
 });
